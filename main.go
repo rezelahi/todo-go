5106a2fb6b759c462a2febe0165ead9859d53aad
Reza Elahi <68402981+rezelahi@users.noreply.github.com>
2025-03-28 19:42:23 +0330

    implement deleteTodo endpoint to remove todos by ID

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
 	app.Patch("/api/todos/:id", updateTodo)
-	// app.Delete("/api/todos/:id", deleteTodo)
+	app.Delete("/api/todos/:id", deleteTodo)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -122,4 +122,22 @@ func updateTodo(c *fiber.Ctx) error {
 
 	return c.Status(201).JSON(fiber.Map{"success": true})
 
+}
+
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Todo ID is Invalid"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
