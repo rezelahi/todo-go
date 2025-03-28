56461a0e558d938886159f091ed0f774b94d5ef4
Reza Elahi <68402981+rezelahi@users.noreply.github.com>
2025-03-29 00:18:00 +0330

    add TodoForm, TodoList, and TodoItem components to manage tasks in the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import { Container, Stack } from "@chakra-ui/react"
 import Navbar from "@/components/Navbar"
+import TodoForm from "@/components/TodoForm"
+import TodoList from "@/components/TodoList"
 
 function App() {
 
@@ -7,8 +9,8 @@ function App() {
     <Stack h="100vh" >
       <Navbar />
       <Container>
-        {/* <TodoForm />
-        <TodoList /> */}
+        <TodoForm />
+        <TodoList />
       </Container>
     </Stack>
   )
