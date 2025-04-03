3a51ee01f53f8f016d002c717c89ecd6e5339af3
Reza Elahi <68402981+rezelahi@users.noreply.github.com>
2025-04-03 12:02:38 +0330

    refactor TodoList component to use react-query for data fetching

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,60 +1,64 @@
 import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
-import { useState } from "react";
 import TodoItem from "@/components/TodoItem";
+import { useQuery } from "@tanstack/react-query";
+
+export type Todo = {
+  _id: number;
+  body: string;
+  completed: boolean;
+};
 
 const TodoList = () => {
-	const [isLoading, setIsLoading] = useState(false);
-	const todos = [
-		{
-			_id: 1,
-			body: "Buy groceries",
-			completed: true,
-		},
-		{
-			_id: 2,
-			body: "Walk the dog",
-			completed: false,
-		},
-		{
-			_id: 3,
-			body: "Do laundry",
-			completed: false,
-		},
-		{
-			_id: 4,
-			body: "Cook dinner",
-			completed: true,
-		},
-	];
-	return (
-		<>
-			<Text fontSize={"4xl"} textTransform={"uppercase"} fontWeight={"bold"} textAlign={"center"} my={2}
-            bgGradient="to-l"
-            gradientFrom="blue.800"
-            gradientTo="blue.400"
-            bgClip="text"
-            >
-				Today's Tasks
-			</Text>
-			{isLoading && (
-				<Flex justifyContent={"center"} my={4}>
-					<Spinner size={"xl"} />
-				</Flex>
-			)}
-			{!isLoading && todos?.length === 0 && (
-				<Stack alignItems={"center"} gap='3'>
-					<Text fontSize={"xl"} textAlign={"center"} color={"gray.500"}>
-						All tasks completed! 🤞
-					</Text>
-					<img src='/go.png' alt='Go logo' width={70} height={70} />
-				</Stack>
-			)}
-			<Stack gap={3}>
-				{todos?.map((todo) => (
-					<TodoItem key={todo._id} todo={todo} />
-				))}
-			</Stack>
-		</>
-	);
+  const { data: todos, isLoading } = useQuery<Todo[]>({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/todos");
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data || [];
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+  return (
+    <>
+      <Text
+        fontSize={"4xl"}
+        textTransform={"uppercase"}
+        fontWeight={"bold"}
+        textAlign={"center"}
+        my={2}
+        bgGradient="to-l"
+        gradientFrom="blue.800"
+        gradientTo="blue.400"
+        bgClip="text"
+      >
+        Today's Tasks
+      </Text>
+      {isLoading && (
+        <Flex justifyContent={"center"} my={4}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+      {!isLoading && todos?.length === 0 && (
+        <Stack alignItems={"center"} gap="3">
+          <Text fontSize={"xl"} textAlign={"center"} color={"gray.500"}>
+            All tasks completed! 🤞
+          </Text>
+          <img src="/go.png" alt="Go logo" width={70} height={70} />
+        </Stack>
+      )}
+      <Stack gap={3}>
+        {todos?.map((todo) => (
+          <TodoItem key={todo._id} todo={todo} />
+        ))}
+      </Stack>
+    </>
+  );
 };
 export default TodoList;
