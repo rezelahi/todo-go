55efbef0ddb87091ea431d960703fe9ceb5bcdeb
Reza Elahi <68402981+rezelahi@users.noreply.github.com>
2025-04-03 13:45:31 +0330

    import Provider component from the correct path in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,8 @@
-import { Provider } from "@/components/ui/provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { Provider } from "@/components/ui/provider.tsx";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
