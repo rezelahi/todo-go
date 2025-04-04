9d7b757e1f19455a46123ee6f47998fc92de402b
Reza Elahi <68402981+rezelahi@users.noreply.github.com>
2025-04-04 13:28:05 +0330

    comment out BASE_URL definition in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import Navbar from "@/components/Navbar";
 import TodoForm from "@/components/TodoForm";
 import TodoList from "@/components/TodoList";
 
-export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+// export const BASE_URL =
+//   import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
   return (
